323ceaa55c7d0edc76a9d3cb901e8ce3b62a563f
Mark Gire <magire01@gmail.com>
2020-07-20 20:51:48 -0400

    Added inquire and fs variables and made sure npms were installed

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log("test");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+//Application Name
+//Purpose of app?
+//Application Description
+//Application HyperLink
+//Would you like to add images? Y/N
+//Any important information the user needs to know before using?
+//Application Author
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the name of the App?",
+        name: "appName"
+    },
+    {
+        type: "input",
+        message: "What is the purpose of this app?",
+        name: "appPurpose"
+    },
+    {
+        type: "input",
+        message: "Please enter a description of this app",
+        name: "appDescription"
+    },
+    {
+        type: "input",
+        message: "Please enter the deployed link to this app",
+        name: "appLink"
+    },
+    {
+        type: "list",
+        message: "Would you like to add images?",
+        name: "appImg",
+        choices: ["yes", "no"]
+    },
+    {
+        type: "input",
+        message: "Please include any information the user needs before using the app",
+        name: "appInfo"
+    },
+    {
+        type: "input",
+        message: "Please enter the Author's name",
+        name: "appAuthor"
+    }
+])
+
+
